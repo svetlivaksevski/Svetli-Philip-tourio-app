09817fe4808a11632eec36377f32d83328927906
philfreshman <philipswiezak@gmail.com>
2024-04-10 11:06:10 +0200

    add validatino and required fields

--- components/Form.js
+++ components/Form.js
@@ -1,35 +1,35 @@
-import styled from "styled-components";
-import { StyledButton } from "./StyledButton.js";
+import styled from "styled-components"
+import { StyledButton } from "./StyledButton.js"
 
 export const FormContainer = styled.form`
   display: grid;
   gap: 0.5rem;
-`;
+`
 
 export const Input = styled.input`
   padding: 0.5rem;
   font-size: inherit;
   border: 1px solid #053b50;
   border-radius: 0.5rem;
-`;
+`
 
 export const Textarea = styled.textarea`
   font-family: inherit;
   border: 1px solid #053b50;
   border-radius: 0.5rem;
   padding: 0.5rem;
-`;
+`
 
 export const Label = styled.label`
   font-weight: bold;
-`;
+`
 
 export default function Form({ onSubmit, formName, defaultData }) {
   function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData);
-    onSubmit(data);
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const data = Object.fromEntries(formData)
+    onSubmit(data)
   }
 
   return (
@@ -39,6 +39,7 @@ export default function Form({ onSubmit, formName, defaultData }) {
         id="name"
         name="name"
         type="text"
+        required
         defaultValue={defaultData?.name}
       />
       <Label htmlFor="image-url">Image Url</Label>
@@ -46,6 +47,7 @@ export default function Form({ onSubmit, formName, defaultData }) {
         id="image-url"
         name="image"
         type="text"
+        required
         defaultValue={defaultData?.image}
       />
       <Label htmlFor="location">Location</Label>
@@ -53,6 +55,8 @@ export default function Form({ onSubmit, formName, defaultData }) {
         id="location"
         name="location"
         type="text"
+        required
+        maxLength={30}
         defaultValue={defaultData?.location}
       />
       <Label htmlFor="map-url">Map Url</Label>
@@ -60,6 +64,7 @@ export default function Form({ onSubmit, formName, defaultData }) {
         id="map-url"
         name="mapURL"
         type="text"
+        required
         defaultValue={defaultData?.mapURL}
       />
       <Label htmlFor="description">Description</Label>
@@ -68,11 +73,12 @@ export default function Form({ onSubmit, formName, defaultData }) {
         id="description"
         cols="30"
         rows="10"
+        maxLength={100}
         defaultValue={defaultData?.description}
       ></Textarea>
       <StyledButton type="submit">
         {defaultData ? "Update place" : "Add place"}
       </StyledButton>
     </FormContainer>
-  );
+  )
 }
